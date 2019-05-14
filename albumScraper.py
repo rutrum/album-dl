4830f61c21242af0f3ca3c476d3d7bbcd5125d79
David Purdum <purdum41@gmail.com>
2019-05-14 10:24:03 -0400

    Add alternative genre scraping method

--- albumScraper.py
+++ albumScraper.py
@@ -3,15 +3,21 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+import sys
+
 def main():
 
-    page = requests.get('https://en.wikipedia.org/wiki/The_Divine_Wings_of_Tragedy')
+    if len(sys.argv) < 2:
+        print("Need wikipedia url as argument.")
+    else:
 
-    soup = BeautifulSoup(page.text, "lxml")
+        page = requests.get(sys.argv[1])
 
-    data = get_album_data(soup)
+        soup = BeautifulSoup(page.text, "lxml")
 
-    print(data)
+        data = get_album_data(soup)
+
+        print(data)
 
 def get_album_data(page):
 
@@ -25,16 +31,27 @@ def get_album_data(page):
     yearElement = table.find("td", class_="published")
     data["year"] = re.findall("[0-9]+", str(yearElement))[0]
 
-    # First find the text with "Genre"
-    genreLink = table.find("a", title="Music genre")
-    # Then look at the next sibling of parent and find the first li element
-    data["genre"] = genreLink.parent.next_sibling.find("li").string
+    data["genre"] = get_genre(table)
 
     data["tracks"] = get_titles(page)
 
     data["trackTotal"] = len(data["tracks"])
 
     return data
+
+def get_genre(table):
+
+    # First find the text with "Genre"
+    genreLink = table.find("a", title="Music genre")
+
+    # Then look at the next sibling of parent and find the first li element
+    genreLi = genreLink.parent.next_sibling.find("li")
+    if genreLi:
+        return genreLi.string
+    
+    # If that didn't work, its a comma seperated list of links
+    return genreLink.parent.next_sibling.a.string
+
 
 def get_titles(page):
 
@@ -45,6 +62,8 @@ def get_titles(page):
 
     for row in rows:
         # Find strings in quotes that follow numbers with periods afterwards
+        # This will ignore any extra content next to title name
+        # or 'parts' of songs (i.e. 2112 by Rush)
         title = re.findall("[0-9][.].*[\"](.*)[\"]", row.get_text())
         if title:
             titles.append(title)
